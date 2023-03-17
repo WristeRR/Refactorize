2c6110b8d824de6f9fa4b94dd78665ae668aea35
WristeRR <abhishek.219302323@muj.manipal.edu>
2023-03-17 10:47:30 +0530

    first commmit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Nav from './components/Navbar'
+
+function App() {
+  return (
+    <div className="Header">
+      <Nav />
+      <h1 className='heading'>REFACTORIZE();</h1>
+    </div>
+  )
+}
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Nav(){
+
+    return(
+         <nav className='navbar'>
+            <h3>Home</h3>
+            <h3>About</h3>
+            <h3>Team</h3>
+      </nav>
+    )
+}

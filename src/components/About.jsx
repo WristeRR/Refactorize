4cfbd4d9d7f764a0cbb12dda29b2fea67b28ba19
WristeRR <abhishek.219302323@muj.manipal.edu>
2023-03-23 02:16:03 +0530

    some minor changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,7 +10,7 @@ export default function About() {
           hosting meaningful and brainstorming events for the brightest minds of
           our community. We are thrilled to announce the opening of
           "REFACTORIZE();", our own annual techfest where we give everyone a
-          chance to learn and execute, even if you are a newbi. Get ready
+          chance to learn and execute, even if you are a newbie. Get ready
           because your brain is getting a gym membership for 3 days! It might
           get tiring, but it'll all be worth it!
           <br />6 Events, Lot's of CODING and LEARNING. Hackathon, Panel talk,

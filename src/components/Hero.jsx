e360f182435669b016941ff18df4cf9a76af03a4
Lakshya <lakshyam38@gmail.com>
2023-03-24 17:11:40 +0530

    New header & nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Logo from '../assets/RefactorizeTypo.svg'
+import React from "react";
+import Logo from "../assets/RefactorizeTypo.svg";
+import "../styles.scss";
 
-
-
-export default function Hero(){
-    return(
-        <div className="Header" id='Hero'>
-              <center><img className='refactorize' src={Logo} alt="logo"/></center>
-              <center><a href='https://forms.gle/MzbmynedSCTQSBrh8' target='_blank'><button className='button-64'><span>Sponsor Us</span></button></a></center>
+export default function Hero() {
+  return (
+    <div className="Header" id="Hero">
+      <h1 className="glitch" data-text="refactorize();">
+        {"refactorize();"}
+      </h1>
+      <p className="hero-subheading">
+        31<sup>st</sup> March - 2<sup>nd</sup> April
+      </p>
+      <div>
+        <a href="https://forms.gle/MzbmynedSCTQSBrh8" target="_blank">
+          <button className="btn-sponsor">
+            <span>Sponsor Us</span>
+          </button>
+        </a>
+        <a href="https://forms.gle/MzbmynedSCTQSBrh8" target="_blank">
+          <button className="btn-register">
+            <span>Register</span>
+          </button>
+        </a>
       </div>
-    )
+    </div>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import {BrowserRouter} from 'react-router-dom'
-import {HashLink as Link} from'react-router-hash-link'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import { HashLink as Link } from "react-router-hash-link";
 
-export default function Nav(){
-
-    return(
-      <BrowserRouter>
-         <nav className='navbar'>
-          <div className='void'>
-          </div>
-            <div className='nav-items'>
-             <Link className='f1' to='#Hero' smooth><span>Home</span></Link>
-             <Link className='f1' to='#About'smooth><span>About</span></Link>
-             <Link className='f1' to='#Sponsors'smooth><span>Sponsors</span></Link>
-             <Link className='f1' to='#Events'smooth><span>Events</span></Link>
-             <a href='https://www.randomizemuj.com/team' className='f1'><span>Team</span></a>
-            </div>
+export default function Nav() {
+  return (
+    <BrowserRouter>
+      <nav className="navbar">
+        {/* <div className='void'> */}
+        {/* </div> */}
+        <div className="nav-items">
+          <Link className="nav-link" to="#Hero" smooth>
+            <span>Home</span>
+          </Link>
+          <Link className="nav-link" to="#About" smooth>
+            <span>About</span>
+          </Link>
+          <Link className="nav-link" to="#Sponsors" smooth>
+            <span>Sponsors</span>
+          </Link>
+          <Link className="nav-link" to="#Events" smooth>
+            <span>Events</span>
+          </Link>
+          <a href="https://www.randomizemuj.com/team" className="nav-link">
+            <span>Team</span>
+          </a>
+        </div>
       </nav>
-      </BrowserRouter>
-    )
+    </BrowserRouter>
+  );
 }
